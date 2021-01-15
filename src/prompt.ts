d06bf24089106f70c1388e439381c5e8236867a7
Bradley Gaynor <BradleyGaynor@northwesternmutual.com>
2021-01-15 11:02:21 -0800

    update ci to take name

--- src/prompt.ts
+++ src/prompt.ts
@@ -6,14 +6,25 @@ interface CustomPrompts {
 }
 
 const customPrompts: CustomPrompts = {
-  module: [{
-    name: 'name',
-    message: 'what is the name of your module?'
-  }]
+  module: []
 }
 
 async function getCustomPrompts (template: string) {
   return customPrompts[template] ?? [] // todo: read from template
+}
+
+async function getName () {
+  if (argv._[0]) {
+    return argv._[0]
+  } else {
+    const response = await inquirer.prompt([
+      {
+        name: 'name',
+        message: 'what is the name of your project?'
+      }
+    ])
+    return response.name
+  }
 }
 
 async function getTemplate () {
@@ -34,6 +45,7 @@ async function getTemplate () {
 
 export async function getTemplateParams () {
   const template = await getTemplate()
+  const name = await getName()
 
   const templatePrompts = await getCustomPrompts(template)
   const answers = await inquirer.prompt([
@@ -41,6 +53,7 @@ export async function getTemplateParams () {
   ])
   return {
     template,
+    name,
     ...answers
   }
 }
